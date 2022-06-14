611cd717fca5f54b04ebf7d9563918249cb2b57c
Gokulcasper <ronitcasper215@gmail.com>
2022-06-14 22:32:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+document.title = "DOM Document";
+console.log(document.head);
+console.dir(document.head);
+console.log(document.body);
+console.log(document);
+console.dir(document);
+
+let myDoc = document;
+document.body.children[0].children[0].innerText =
+  "Learn HTML DOM in JAVASCRIPT";
